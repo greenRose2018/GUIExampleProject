745b4d9403d2ba2cfcefbb9eede0f6e0028683ba
greenRose2018 <brifun13@gmail.com>
2017-11-13 08:13:05 -0700

    set runner

--- src/gui/controller/GUIRunner.java
+++ src/gui/controller/GUIRunner.java
@@ -2,5 +2,10 @@ package gui.controller;
 
 public class GUIRunner
 {
-
+	public static void main (String [] args)
+	{
+		GUIAppController guiApp = new GUIAppController();
+		guiApp.start();
+	}
+		
 }
