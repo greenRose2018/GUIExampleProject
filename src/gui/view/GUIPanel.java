7c94eb6bd277e17ea31b62ca9382a858c787749e
greenRose2018 <brifun13@gmail.com>
2017-11-15 08:49:17 -0700

    added blankline

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -8,6 +8,7 @@ import javax.swing.SpringLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class GUIPanel extends JPanel
 {
 	private GUIAppController appController;
